3969f823450783439517a93e0dbce08cd65dc613
FreitasLeiloeiro <consultaacao@gmail.com>
2026-03-27 17:40:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,5 @@
-import express from "express";
-import { chromium } from "playwright";
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-app.get("/consulta", async (req, res) => {
-
-const nome = req.query.nome;
-const cpf = req.query.cpf;
-
-let resultados = [];
-
-const browser = await chromium.launch({
-headless: true
-});
-
-const page = await browser.newPage();
-
-try {
-
-await page.goto("https://esaj.tjsp.jus.br/cpopg/open.do");
-
-await page.waitForTimeout(2000);
-
-resultados.push({
-tribunal: "TJSP",
-status: "consulta iniciada",
-nome: nome,
-cpf: cpf
-});
-
-} catch (e) {
-
-console.log(e);
-
-}
-
-await browser.close();
-
-res.json({
-status: "ok",
-resultados
-});
-
-});
-
-app.listen(3000, () => {
-console.log("Servidor rodando");
+app.listen(PORT, () => {
+console.log("Servidor rodando na porta", PORT);
 });
