61328288cc67a5d049f7484f29353032d0eb1742
FreitasLeiloeiro <consultaacao@gmail.com>
2026-03-30 10:46:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,62 @@
 import express from "express";
 import cors from "cors";
+import fetch from "node-fetch";
+import * as cheerio from "cheerio";
 
 const app = express();
 
 app.use(cors());
 
 app.get("/", (req,res)=>{
-res.send("API Consulta Processos Online");
+res.send("API Consulta Processos");
 });
 
-app.get("/processo",(req,res)=>{
+app.get("/processo", async (req,res)=>{
 
 const numero = req.query.numero;
+
+try{
+
+const url = `https://esaj.tjsp.jus.br/cpopg/show.do?processo.numero=${numero}`;
+
+const resposta = await fetch(url);
+
+const html = await resposta.text();
+
+const $ = cheerio.load(html);
+
+let classe = $("#classeProcesso").text().trim();
+let assunto = $("#assuntoProcesso").text().trim();
+
+if(!classe){
+classe = "Classe não identificada";
+}
+
+if(!assunto){
+assunto = "Assunto não identificado";
+}
 
 const resultado = {
 tribunal:"TJSP",
 processo:numero,
-classe:"Execução de título extrajudicial",
-assunto:"Cobrança bancária",
-data:"2023-05-10",
-banco:"Banco do Brasil",
-acao_contra_banco:true
+classe:classe,
+assunto:assunto,
+banco:"não identificado"
 };
 
 res.json({
 status:"ok",
 resultado
 });
+
+}catch(e){
+
+res.json({
+status:"erro",
+mensagem:"Erro ao consultar TJSP"
+});
+
+}
 
 });
 
